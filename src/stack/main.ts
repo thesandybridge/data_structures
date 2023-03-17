f1131eb392523901105330099de0e889f3f19c4f
Matt Miller <mattmillerf@pm.me>
2023-03-17 19:55:46 -0400

    stack

---
+++ src/stack/main.ts
@@ -0,0 +1,44 @@
+type Node<T> = {
+    value: T,
+    prev?: Node<T>
+}
+
+export default class Stack<T> {
+    public length: number;
+    private head?: Node<T>;
+
+    constructor() {
+        this.head = undefined;
+        this.length = 0;
+    }
+
+    push(item: T): void {
+        const node = {value: item} as Node<T>;
+        this.length++;
+
+        if (!this.head) {
+            this.head = node;
+            return;
+        }
+
+        node.prev = this.head;
+        this.head = node;
+    }
+
+    pop(): T | undefined {
+        const head = this.head as Node<T>;
+        this.length = Math.max(0, this.length - 1);
+        if (this.length === 0) {
+            this.head = undefined;
+            return head?.value;
+        }
+
+        this.head = head.prev;
+        // free
+        return head.value;
+    }
+
+    peek(): T | undefined {
+        return this.head?.value;
+    }
+}
